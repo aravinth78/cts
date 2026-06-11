19cc58cf664e81f7653f24272cddb3bc9242cc8c
root <root@srv-d8imnpr7uimc73av4eb0-hibernate-85d6488f88-lkvcr>
2026-06-11 18:38:54 +0000

    Uploaded via aravinth78 — 2026-06-12 12:08 AM IST

---
+++ Digital-Nurture-JavaFSE/MODULE-3/Java/Exercise15/StringReversal.java
@@ -0,0 +1,17 @@
+﻿import java.util.Scanner;
+
+public class ReverseText {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String input = sc.nextLine();
+
+        char[] chars = input.toCharArray();
+        String rev = "";
+        for (int i = chars.length - 1; i >= 0; i--)
+            rev += chars[i];
+
+        System.out.println("Reversed: " + rev);
+        sc.close();
+    }
+}
---
+++ Digital-Nurture-JavaFSE/MODULE-3/Java/Exercise27/LambdaSortDemo.java
@@ -0,0 +1,15 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+
+public class SortWithLambda {
+    public static void main(String[] args) {
+        List<String> cities = new ArrayList<>();
+        cities.add("Chennai");
+        cities.add("Mumbai");
+        cities.add("Delhi");
+
+        System.out.println("Before: " + cities);
+        cities.sort((a, b) -> a.compareTo(b));
+        System.out.println("After: " + cities);
+    }
+}
