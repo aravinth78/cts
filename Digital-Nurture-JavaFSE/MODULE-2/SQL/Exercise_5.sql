1934d6d842af83439d017f76530bd3afe1952ca2
root <root@srv-d8imnpr7uimc73av4eb0-hibernate-85d6488f88-lkvcr>
2026-06-17 14:38:54 +0000

    Uploaded via aravinth78 — 2026-06-17 08:08 PM IST

---
+++ Digital-Nurture-JavaFSE/MODULE-2/SQL/Exercise_5.sql
@@ -0,0 +1,9 @@
+﻿SELECT
+    usr.location,
+    COUNT(DISTINCT evt.id) AS total_events
+FROM Users usr
+INNER JOIN Events evt
+    ON usr.location = evt.location
+GROUP BY usr.location
+ORDER BY total_events ASC
+LIMIT 3;
